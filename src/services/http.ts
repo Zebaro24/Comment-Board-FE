27a37b7ec553f9ea601bca918e8ece277f26b494
Zebaro <denok100100@gmail.com>
2025-10-30 19:15:20 +0100

    fix: change default value

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiRoot = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000';
+const apiRoot = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
 const normalizedRoot = apiRoot.replace(/\/$/, '');
 
 export const http = axios.create({
